0a97683510d9f9f552e51ce6ad2786082ec8071e
stroom <stroooooom@gmail.com>
2017-09-28 13:13:31 +0300

    previous version found set of symbols in string, but not a word; this ver. checks if found a word or just matched the same number of characters in word (eg 'do' was found in 'dog', what was wrong), which length might be more than our one

--- C_01/c_1.c
+++ C_01/c_1.c
@@ -12,11 +12,10 @@ int arglen(char *word);
 int *countOccurrences(char *word, int word_len, char *filename, int *articleCount);
 void cleanBuffer(char *buffer, int size);
 int compare(char *str1, char *str2);
-int disp(int *occr, int *artclCount);
+int disp(int *occr, int artclCount);
 
 int main(int agrc, char **argv)
 {
-	setbuf(stdout, NULL);
 	if (agrc != 3)
 	{
 		printf("\nInput_Error: You must enter only 3 arguments");
@@ -30,8 +29,14 @@ int main(int agrc, char **argv)
 
     int articleCount = 0;
 	int *n = countOccurrences(word, word_len, argv[2], &articleCount);
-//	printf("\nacticle count = %d", articleCount);
-	disp(n, &articleCount);
+    if (n != NULL)
+    {
+        disp(n, articleCount);
+        free(n);
+    }
+    else
+        printf("\nОшибка при открытии файла или выделении памяти");
+    
     printf("\n");
 	return 0;
 }
@@ -42,8 +47,6 @@ int *countOccurrences(char *word, int word_len, char *filename, int *articleCoun
 	if (f == NULL)
 		return NULL;
 
-//	printf("\nword: %s", word);
-//	printf("\nword_len = %d", word_len);
 	char buffer[word_len];
 	cleanBuffer(buffer, word_len);
 	int b_id = 0;
@@ -53,40 +56,56 @@ int *countOccurrences(char *word, int word_len, char *filename, int *articleCoun
 	int *occr = (int*) malloc(*articleCount * sizeof(int));
 	occr[*articleCount-1] = 0; //кол-во вхождений в слове по номеру абзаца
 
-	char l = '*'; //letter
+	char current = '*'; //current symbol
 	char previous = '*';
 	int res = 0;
 	while(!feof(f))
 	{
-		previous = l;
-		res = fscanf(f, "%c", &l);
-		l = tolower(l);
+		previous = current;
+		res = fscanf(f, "%c", &current);
+		current = tolower(current);
 		if (feof(f))
 		{
 			occrCount += compare(buffer, word);
+            if (compare(buffer, word) == 1)
+                printf(" <<");
 			break;
 		}
-		else if ((ispunct(l) == 0) && (isspace(l) == 0))
+		if ((ispunct(current) == 0) && (isspace(current) == 0))
+        {
 			if (b_id < word_len)
 			{
-				buffer[b_id] = l;
+				buffer[b_id] = current;
 				b_id++;
 			}
 			else
 			{
-				cleanBuffer(buffer, word_len);
-				b_id = 0;
-				buffer[b_id] = l;
-				b_id++;
+                while ((ispunct(current) == 0) && (isspace(current) == 0) && (!feof(f)))
+                {
+                    if (!feof(f))
+                        fscanf(f, "%c", &current);
+                    else
+                        break;
+                }
+                cleanBuffer(buffer, word_len);
+                b_id = 0;
 			}
-		else if ((ispunct(l) != 0) || (isspace(l) != 0))
+        }
+		if ((ispunct(current) != 0) || (isspace(current) != 0))
 		{
 			occrCount += compare(buffer, word);
+            if (compare(buffer, word) == 1)
+            {
+                printf("\ncur word: ");
+                for(int i = 0; i < word_len; i++)
+                    printf("%c", buffer[i]);
+                printf(" <<");
+            }
 			cleanBuffer(buffer, word_len);
 			b_id = 0;
 		}
 
-		if ((previous == '\n') && (l == '\n'))
+		if ((previous == '\n') && (current == '\n'))
 		{
 			occr[*articleCount-1] = occrCount;
 			occrCount = 0;
@@ -97,27 +116,27 @@ int *countOccurrences(char *word, int word_len, char *filename, int *articleCoun
 		}
 	}
 	occr[*articleCount-1] = occrCount;
-//	printf("\n\nКол-во абзацев: %d", *articleCount);
-//	printf("\nКол-во вхождений: ");
-//	for(int i = 0; i < *articleCount; i++)
-//		printf("\n#%d: %d", i, occr[i]);
+	printf("\n\nКол-во абзацев: %d", *articleCount);
+	printf("\nКол-во вхождений: ");
+	for(int i = 0; i < *articleCount; i++)
+		printf("\n#%d: %d", i+1, occr[i]);
 
 	return occr;
 }
 
-int disp(int *occr, int *artclCount)
+int disp(int *occr, int artclCount)
 {
 	float avg = 0;
-	for(int i = 0; i < *artclCount; i++)
+	for(int i = 0; i < artclCount; i++)
 		avg +=occr[i];
 //	printf("\nСумма вхождений: %d", (int) avg);
-	avg /= *artclCount;
+	avg /= artclCount;
 //	printf("\nСр. арифм. вхождений: %f", avg);
 	float dispersion = 0;
-	for(int i = 0; i < *artclCount; i++)
-		dispersion += pow(occr[i]-avg, 2);
-	if (*artclCount > 1)
-		dispersion = sqrt(dispersion/(*artclCount-1));
+	for(int i = 0; i < artclCount; i++)
+		dispersion += pow((occr[i]-avg), 2);
+	if (artclCount > 1)
+		dispersion = sqrt(dispersion/(float)(artclCount-1));
 	else
 		dispersion = 0;
 	printf("\nДисперсия вхождений: %f", dispersion);
